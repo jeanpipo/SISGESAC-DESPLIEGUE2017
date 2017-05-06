ca95459c38fd82bbb6392f6d0ad1fbf11673d87b
jeanpipo <root@localhost.localdomain>
2017-05-06 18:05:12 -0430

    se acomodar algunos acentos en el modulo de inscripcion nuevo ingreso

--- modulos/persona/vista/pdf/nuevoIngresoNoInscrito.php
+++ modulos/persona/vista/pdf/nuevoIngresoNoInscrito.php
@@ -11,13 +11,13 @@
 		$pdf->SetTopMargin(20);
 
 		$pdf->SetFont('Arial','B',14);
-		$pdf->Cell(0,10,"LISTA DE ESTUDIANTES QUE YA ENCUENTRAN REGISTRADAS ",0,1,'C',true);
-		$pdf->Cell(0,10,"POR LO QUE DEBEN SER REGISTRADAS MANUALMENTE",0,1,'C',true);
-		$pdf->Cell(0,10,"EN EL MODULO DE ESTUDIANTES",0,1,'C',true);
+		$pdf->Cell(0,10,"LISTA DE ESTUDIANTES QUE YA SE ENCUENTRAN REGISTRADOS ",0,1,'C',true);
+		$pdf->Cell(0,10,"POR LO QUE DEBEN SER REGISTRADOS MANUALMENTE",0,1,'C',true);
+		$pdf->Cell(0,10,"EN EL MÓDULO DE ESTUDIANTES",0,1,'C',true);
 		$pdf->Ln();
 
 		$pdf->SetFont('Arial','B',11);
-		$pdf->Cell(20,7,"Cedula",1,0,'C',true);
+		$pdf->Cell(20,7,"Cédula",1,0,'C',true);
 		$pdf->SetFillColor(255,255,255);
 		$pdf->SetTextColor(0);
 		$pdf->SetFont('Arial','B',11);
@@ -25,7 +25,7 @@
 		$pdf->SetFont('Arial','B',11);
 		$pdf->Cell(50,7,"Nombre",1,0,'C',true);
 		$pdf->SetFont('Arial','B',11);
-		$pdf->Cell(70,7,"e-mail",1,0,'C',true);
+		$pdf->Cell(70,7,"E-mail",1,0,'C',true);
 
 		$pdf->Ln();
 			
